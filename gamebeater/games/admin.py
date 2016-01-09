8ed15923e52f109ef0bd166f645f45bf2e7121a3
Steven Miller <msm@gatech.edu>
2016-01-09 14:17:28 -0500

    Add some extra admin stuff

--- gamebeater/games/admin.py
+++ gamebeater/games/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 from .models import Game, Console, Genre
 
 class GameAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Start time', {'fields': ['get_goal_start_time']}),
+		('Status', {'fields': ['get_goal_status']}),
+		('Complete time', {'fields': ['get_goal_complete_time']}),
+		('Goal', {'fields': ['get_goal_text']}),
+	]
+
 	list_display = ('title', 'get_goal_start_time', 'get_goal_status', 'get_goal_complete_time', 'get_goal_text')
+	search_fields = ['title']
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(Console)
